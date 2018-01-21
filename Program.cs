42c564b8aa8e79f9fef8bbbd41690817fba3c3a7
Mikhail Gorelov <leofesk@gmail.com>
2018-01-21 19:33:39 +0300

    Added project.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FindAverageValueOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = WorkWithArray.Generate(Convert.ToInt32(args[0]));
+            Console.WriteLine("Average Value: " + WorkWithArray.FindAverage(array));
+        }
+    }
+}
---
+++ WorkWithArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FindAverageValueOfArray
+{
+    public class WorkWithArray
+    {
+        public static int[] Generate(int size)
+        {
+            int[] array = new int[size];
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 99);
+            }
+
+            return array;
+        }
+
+        public static int FindAverage(int[] array)
+        {
+            int tmp = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                tmp = tmp + array[i];
+            }
+
+            return tmp / array.Length;
+        }
+    }
+}
