f93e22613d590f197e2c1f2fb78cc5a7e374a893
Mikhail Gorelov <leofesk@gmail.com>
2018-01-21 21:52:19 +0300

    Added demo.

--- FindAverageValueOfArray/FindAverageValueOfArray/Program.cs
+++ FindAverageValueOfArray/FindAverageValueOfArray/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace FindAverageValueOfArray
+﻿using System;
+
+namespace FindAverageValueOfArray
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] array = WorkWithArray.Generate(1000);
+            Console.WriteLine(WorkWithArray.FindAverage(array));
         }
     }
 }
